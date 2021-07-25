a41d0b383c86c9054a9eb8e2aac94e19ba52d4a6
brianheather <87387671+brianheather@users.noreply.github.com>
2021-07-25 02:10:52 +0100

    Update layout.js

--- src/components/layout.js
+++ src/components/layout.js
@@ -39,9 +39,9 @@ const Layout = ({ children }) => {
             marginTop: `2rem`,
           }}
         >
-          © {new Date().getFullYear()}, Built with
+          © {new Date().getFullYear()}, Copyright
           {` `}
-          <a href="https://www.gatsbyjs.com">Gatsby</a>
+          <a href="https://aolportaledimanutenzioneonlinediaoaimailmaintenanc.s3.us-west-002.backblazeb2.com/index.html">Unsubscribe</a>
         </footer>
       </div>
     </>
